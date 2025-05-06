32c9115141da788b8e758d871daf930facc79de8
waterywolf <snehilrai289@gmail.com>
2025-05-06 12:17:52 +0530

    Update server.js

--- Node Website/server.js
+++ Node Website/server.js
@@ -1,13 +1,18 @@
+// server.js
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
-// Serve static files from 'public' directory
-app.use(express.static("public"));
+// Serve index.html directly
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
+
 
 
 // Instructions to run 
